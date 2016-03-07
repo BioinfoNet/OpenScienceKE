16c68c983ff9ece4a258d654abd648c54c04e729
Joey Lee <josephkanglee@gmail.com>
2016-03-06 16:55:52 -0800

    added comment in interactive r code

--- examples/interactive-r-map/code/script.R
+++ examples/interactive-r-map/code/script.R
@@ -12,3 +12,6 @@ m = addMarkers(m, lng=-123.252691, lat=49.267570, popup="Here we are at UBC")
 
 # show the map
 print(m)
+
+# if you "export to web" in R Studio --> save it as "index.html" and 
+# it will open up in a webpage :) 
